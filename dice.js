8acbb4d2f5709862cef6a1c517aa5371bf49d2d0
Ali Anwar <ali@alis-mac.local>
2019-10-19 22:10:03 -0700

    Dice: Added roll with pure randomness

--- dice.js
+++ dice.js
@@ -1,11 +1,87 @@
 var fs = require("fs")
+var uuid = require('uuid');
 
 class Dice {
-  constructor() {
-    var uuid = require('uuid');
-    this.uniqueID = uuid.v4();
+  constructor(id) {
+    this.initializeCounts();
+
+    if(id) {
+      this.uniqueID = id;
+      this.readCounts();
+    } else {
+      this.uniqueID = uuid.v4();
+      this.saveCounts();
+    }
+  }
+
+  initialCount() {
+    return 10;
+  }
+
+  initializeCounts() {
     this.counts = this.initialCounts();
-    this.saveCounts();
+    this.sum = this.counts.reduce((a, b) => a + b, 0);
+  }
+
+  static find(id) {
+    var dice = new Dice(id);
+
+    if(dice.valid) {
+      return dice;
+    }
+
+    return null;
+  }
+
+  pureRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  gotNumber(number) {
+    var index = number - this.minForDice();
+
+    this.counts[index] += 1;
+    this.sum += 1;
+    this.saveCounts()
+  }
+
+  minForDice() {
+    return 2;
+  }
+
+  maxForDice() {
+    return 12;
+  }
+
+  nextNumber() {
+    var number = this.pureRandom(this.minForDice(), this.maxForDice());
+
+    this.gotNumber(number);
+
+    return number;
+  }
+
+  splitNumber(number) {
+    var dice1Number = this.pureRandom(1, number - 1);
+    var dice2Number = number - dice1Number;
+
+    if(this.pureRandom(0, 1) == 0) {
+      return [dice1Number, dice2Number];
+    }
+
+    return [dice2Number, dice1Number];
+  }
+
+  roll() {
+    var dice1Number, dice2Number, nextNumber = this.nextNumber();
+
+    [dice1Number, dice2Number] = this.splitNumber(nextNumber);
+
+    return {
+      dice1: dice1Number,
+      dice2: dice2Number,
+      sum: nextNumber
+    };
   }
 
   countsFilePath() {
@@ -14,7 +90,8 @@ class Dice {
 
   data() {
     return {
-      data: this.counts
+      sum: this.sum,
+      counts: this.counts
     }
   }
 
@@ -22,16 +99,38 @@ class Dice {
     return JSON.stringify(this.data());
   }
 
+
+  readCounts() {
+    try {
+      var data = fs.readFileSync(this.countsFilePath());
+      var diceData = JSON.parse(data);
+
+      this.counts = diceData['counts']
+      this.sum = diceData['sum']
+      this.valid = true;
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
   saveCounts() {
-    fs.writeFile(this.countsFilePath(), this.countsToJson(), function(err) {
-      if (err) {
-        return console.error(err);
-      }
-    });
+    try {
+      fs.writeFileSync(this.countsFilePath(), this.countsToJson());
+      this.valid = true;
+    } catch(err) {
+      console.error(err);
+    }
   }
 
   initialCounts() {
-    return [0, 0, 0, 0, 0, 0, 0,0 ,0, 0, 0];
+    var i;
+    var counts = [];
+
+    for (i = 0; i < this.maxForDice(); i++) {
+      counts.push(this.initialCount());
+    }
+
+    return counts;
   }
 
   id() {
--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const Dice = require('./dice')
 const app = express()
 const port = 3000
 
+var jsonError = (message) => {
+  return {
+    error: message
+  }
+}
+
 app.get('/dice/new', (req, res) => {
   var dice = new Dice()
-  res.send(`New dice: ${dice.id()}`)
+  res.json({id: dice.id()})
 })
+
+app.get('/dice/:diceID/roll', (req, res) => {
+  var dice = Dice.find(req.params.diceID);
+
+  if(dice) {
+    res.send(dice.roll())
+  } else {
+    res.status(404).json(jsonError("Dice not found"))
+  }
+})
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to Dice of Catan!')
